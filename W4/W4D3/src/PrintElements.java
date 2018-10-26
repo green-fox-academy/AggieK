5592e2114fb166ed493bba0982fef77f1cf1bc18
Agnes <agnes.koperveisz@yahoo.co.uk>
2018-10-26 11:09:27 +0200

    third commit

---
+++ W4/W4D3/src/PrintElements.java
@@ -0,0 +1,9 @@
+public class PrintElements {
+    public static void main(String[] args) {
+
+        int [] numbers = {4,5,6,7};
+        for (int item: numbers) {
+            System.out.print(item + ", ");
+        }
+    }
+}
