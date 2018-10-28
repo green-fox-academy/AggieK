485e8691c647ecea3fadad8a3a34fabc3fdcef4b
Agnes <agnes.koperveisz@yahoo.co.uk>
2018-10-29 00:37:11 +0100

    Reverse

---
+++ W4/W4D3/src/Reverse.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Reverse {
+
+    public static void main(String[] args) {
+        int [] aj = {3,4,5,6,7};
+        System.out.println("Before reverse: " + Arrays.toString(aj));
+        reverse (aj);
+        System.out.println("Before reverse: " + Arrays.toString(aj));
+    }
+
+    public static void reverse(int[] array) {
+        int[] arrayCopy = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = arrayCopy[array.length - 1 - i];
+        }
+    }
+}
