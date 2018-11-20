0d2b021331c750f6151a953f2f9c872d3186d746
Agnes <agnes.koperveisz@yahoo.co.uk>
2018-11-20 11:56:58 +0100

    BlogPost

--- W8/src/BlogPost.java
+++ W8/src/BlogPost.java
@@ -1,8 +1,36 @@
 public class BlogPost {
-    private String autorname;
+    private String authorname;
     private String title;
     private String text;
     private String publicationDate;
 
+    BlogPost(String authorname, String title, String text, String publicationDate) {
+        this.authorname = authorname;
+        this.title = title;
+        this.text = text;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getAutorname() {
+        return authorname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    @Override
+    public String toString(){
+        return authorname +", "+ title+", "+text+", "+publicationDate;
+    }
 
 }
+
--- W8/src/BlogPostMain.java
+++ W8/src/BlogPostMain.java
@@ -1,3 +1,12 @@
 public class BlogPostMain {
 
+    public static void main(String[] args) {
+        BlogPost myBlogPost01 = new BlogPost("John Doe", "Lorem Ipsum","Lorem ipsum dolor sit amet.", "2000.05.04" );
+        BlogPost myBlogPost02 = new BlogPost("Tim Urban", "Wait but why","A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10." );
+        BlogPost myBlogPost03 = new BlogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump","Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28." );
+
+        System.out.println(myBlogPost01);
+        System.out.println(myBlogPost02);
+        System.out.println(myBlogPost03);
+    }
 }
