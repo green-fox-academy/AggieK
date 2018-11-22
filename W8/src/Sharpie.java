060ad9b6a0366406bcf41097a152e93da24e62a0
Agnes <agnes.koperveisz@yahoo.co.uk>
2018-11-22 13:22:21 +0100

    Sharpie&Pokemon

---
+++ W8/src/Sharpie.java
@@ -0,0 +1,29 @@
+public class Sharpie {
+
+    private String color;
+    private float width;
+    private float inkAmount;
+
+    public Sharpie(String color, float width){
+        this.color = color;
+        this.width = width;
+        inkAmount = 100.0f;
+    }
+
+    public void use(){
+        inkAmount--;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getInkAmount() {
+        return inkAmount;
+    }
+}
+
---
+++ W8/src/SharpieMain.java
@@ -0,0 +1,9 @@
+public class SharpieMain {
+
+    public static void main(String[] args) {
+
+        Sharpie mySharpie01 = new Sharpie("pink",0.5f);
+
+
+    }
+}
