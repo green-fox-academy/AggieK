2a38755978c05b2667b37f32fe41e4e3ef984f9c
Agnes <agnes.koperveisz@yahoo.co.uk>
2018-12-20 12:01:53 +0100

    Add new task error handling

--- W12/TodoApp/src/TodoApp.java
+++ W12/TodoApp/src/TodoApp.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
@@ -6,21 +7,23 @@ public class TodoApp {
 
     public static void main(String[] args) {
         TodoList todolist = new TodoList();
-
-        if (args.length == 0){
-            System.out.println(TodoList.printUsage());
-        }else if (args[0].equals("-l")) {
-            System.out.println(todolist.printTasks());
-        }else if (args[0].equals("-a")) {
-            todolist.add(args[1]);
-        }else if (args[0].equals("-r")) {
-            todolist.remove(args[1]);
-        } else if (args[0].equals("-c")){
-            todolist.completed(args[1]);
-        } else{
-            System.out.println("Sorry, we have no such task! Available tasks listed below:");
-            System.out.println(TodoList.printUsage());
+        try {
+            if (args.length == 0) {
+                System.out.println(TodoList.printUsage());
+            } else if (args[0].equals("-l")) {
+                System.out.println(todolist.printTasks());
+            } else if (args[0].equals("-a")) {
+                todolist.add(args[1]);
+            } else if (args[0].equals("-r")) {
+                todolist.remove(args[1]);
+            } else if (args[0].equals("-c")) {
+                todolist.completed(args[1]);
+            } else {
+                System.out.println("Sorry, we have no such task! Available tasks listed below:");
+                System.out.println(TodoList.printUsage());
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Unable to procede: no task provided");
         }
     }
-
 }
