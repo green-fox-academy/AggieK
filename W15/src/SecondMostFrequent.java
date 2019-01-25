0f3d6458b23ce6999640715ebbd83e2114b65924
Agnes <agnes.koperveisz@yahoo.co.uk>
2019-01-25 11:52:24 +0100

    (SecondMostFrequent)second most frequent corrected

--- W15/src/SecondMostFrequent.java
+++ W15/src/SecondMostFrequent.java
@@ -7,8 +7,12 @@ public class SecondMostFrequent {
 
     int[] numbers = {1, 2, 3, 4, 5, 5, 5, 2, 3, 3, 5, 4, 8, 9};
 
-    getTheMostFrequent(createAHashMap(numbers));
+    HashMap<Integer, Integer> aHashMap = createAHashMap(numbers);
+    Map.Entry theMostFrequent = findTheMostFrequent(aHashMap);
+    Map.Entry theSecondMostFrequent = findTheSecondMostFrequent(theMostFrequent, aHashMap);
+    System.out.println(theSecondMostFrequent.getKey());
   }
+
 
   public static HashMap<Integer, Integer> createAHashMap(int[] numbers) {
     HashMap<Integer, Integer> frequencyByNumber = new HashMap<>();
@@ -22,23 +26,19 @@ public class SecondMostFrequent {
     return frequencyByNumber;
   }
 
-  public static void getTheMostFrequent(HashMap<Integer, Integer> frequencyByNumber) {
+  public static Map.Entry findTheMostFrequent(HashMap<Integer, Integer> frequencyByNumber) {
     Map.Entry<Integer, Integer> mostFrequentNumber = null;
-    Map.Entry<Integer, Integer> secondMostFrequentNumber = null;
-
-    for (Map.Entry<Integer, Integer> item : frequencyByNumber.entrySet()) {
+    for (Map.Entry<Integer, Integer> item : frequencyByNumber.entrySet())
       if (mostFrequentNumber == null) {
         mostFrequentNumber = item;
       } else if (item.getValue() > mostFrequentNumber.getValue()) {
         mostFrequentNumber = item;
-      } else if (item.getValue() < mostFrequentNumber.getValue() && secondMostFrequentNumber == null) {
-        secondMostFrequentNumber = item;
-      } else if
-      (item.getValue() < mostFrequentNumber.getValue() && item.getValue() > secondMostFrequentNumber.getValue()) {
-        secondMostFrequentNumber = item;
       }
-    }
-    System.out.println("The most frequent number is : " + mostFrequentNumber.getKey() + " , available in the list " + mostFrequentNumber.getValue() + " times.");
-    System.out.println("The second most frequent number is : " + secondMostFrequentNumber.getKey() + " , available in the list " + secondMostFrequentNumber.getValue() + " times.");
+    return mostFrequentNumber;
+  }
+
+  public static Map.Entry findTheSecondMostFrequent(Map.Entry<Integer, Integer> mostFrequentNumber, HashMap<Integer, Integer> frequencyByNumber) {
+    frequencyByNumber.remove(mostFrequentNumber.getKey());
+    return findTheMostFrequent(frequencyByNumber);
   }
 }
