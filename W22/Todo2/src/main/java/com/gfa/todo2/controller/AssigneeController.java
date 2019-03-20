18a3923b4c04314adbab5395baad447c30fa180e
Agnes <agnes.koperveisz@yahoo.co.uk>
2019-03-20 18:25:49 +0100

    (Assignee) Controller&assigneeMain html created

--- W22/Todo2/src/main/java/com/gfa/todo2/controller/AssigneeController.java
+++ W22/Todo2/src/main/java/com/gfa/todo2/controller/AssigneeController.java
@@ -1,7 +1,15 @@
 package com.gfa.todo2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/assignee")
 public class AssigneeController {
+
+  @GetMapping()
+  public String renderAssignee(){
+    return "assigneeMain";
+  }
 }
